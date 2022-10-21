9026855c8883a4f9e61db3386e45a412bfda68e1
jumpb0y <116382072+jumpb0y@users.noreply.github.com>
2022-10-21 16:05:54 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,10 +4,10 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SENAI ZERBINI É NOTA 1000!")
+st.title("BOHEMINAN RHAPSODY É NOTA 1000!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Abacaxi em tudo!")
+st.header("KILLER QUEEN!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
